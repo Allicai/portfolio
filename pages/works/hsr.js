8880951116973c4611b402ed922cd9f65599f060
Vin <vinitp0310@gmail.com>
2024-09-11 22:54:42 -0400

    fleshed out hsr project

--- pages/works/hsr.js
+++ pages/works/hsr.js
@@ -21,31 +21,20 @@ import {
           <Badge>API </Badge> {' '}
           <Badge>Optimization </Badge> 
         </Title>
+
+        
         <Center my={6}>
           <Image src="/images/works/hsr_train.png" alt="banner" />
         </Center>
-        <P>
-          This project was inspired by my experience of various public transportation systems in Europe, Asia, and New York itself
-          as well as my personal struggle with commuting between Boston and New York during holidays. Using the 
-          knowledge I had gained about algorithms and search spaces, and with the information I gathered regarding the infrastructure
-          of the United States, particularly this <Link href="https://www.scientificamerican.com/article/why-high-speed-bullet-trains-wont-work-in-the-u-s-right-now/#:~:text=High-speed%20rail%20could%20reduce,or%20faster%20than%20air%20travel."> article</Link>,
-          I set out to design an algorithm to optimize the placement of high speed rail systems across the continent.
-        </P>
-        <P>
-          The models used in the experiment were Logistic Regression, LSTM, and a 
-          DistilBERT transformer model. I prepared the data and created training, testing, 
-          and validation dataset. The three models performed well, resulting in accuracies of 87%, 92%, and 93% respectively, with
-          the DistilBERT model showing the most promise (likely due to its self-attention mechanism).
-        </P>
-  
+
         <List ml={4} my={4}>
           <ListItem>
             <Meta>Skills</Meta>
-            <span>PyTorch, Keras, Scikit-learn, TensorFlow, Seaborn, NLTK</span>
+            <span>Pandas, GeoPandas, Matplotlib, NetworkX, Shapely, GIS</span>
           </ListItem>
           <ListItem>
             <Meta>Code</Meta>
-            <Link href="https://github.com/Allicai/EmotionClassification">
+            <Link href="https://github.com/Allicai/high-speed-rail">
               GitHub Link
             </Link>
           </ListItem> 
@@ -62,8 +51,36 @@ import {
             <Link href="https://w.atwiki.jp/amembo/">使い方wiki</Link>
           </ListItem> */}
         </List>
+        
+        <P>
+          This project was inspired by my experience of various public transportation systems in Europe, Asia, and New York itself
+          as well as my personal struggle with commuting between Boston and New York during holidays. Using the 
+          knowledge I had gained about algorithms and search spaces, and with the information I gathered regarding the infrastructure
+          of the United States, particularly this <Link href="https://www.scientificamerican.com/article/why-high-speed-bullet-trains-wont-work-in-the-u-s-right-now/#:~:text=High-speed%20rail%20could%20reduce,or%20faster%20than%20air%20travel."> article</Link>,
+          I set out to design an algorithm to optimize the placement of high speed rail systems across the continent.
+        </P>
+        <P>
+        A large part of this project was a data pipeline that I created with metrics on emissions, flight markets, populations and locations of major cities, and an estimated cost of construction.
+        This would inform the algorithm on how optimal each rail system would be, calculating the number of passengers served, the emissions saved, travel times, and so on. The data for this was
+        mainly gathered from U.S. census data, the EPA, the AirLabs API, and the DB1B survey. A special thank you during this process to Sergey from AirLabs in particular, who gave me a student license for this project with
+        50,000 free API calls, which let me search for airport schedules by code, search for nearby airports using latitude and longitude, etc. and compile my dataset with ease.
+        </P>
         <Center my={6}>
-          <Image src="/images/works/distilbert.png" alt="cm" />
+          <Image src="/images/works/hsr_table.png" alt="banner" />
+        </Center>
+        <P>
+        The algorithm for this project is based on uniform-cost search, a variant of Dijkstra&apos;s algorithm. The algorithm systematically expands the search space,
+        prioritizing nodes based on various factors it gets from the data pipeline, ensuring a comprehensive exploration of the solution space. Since this project lacks
+        a heuristic, the exploration of potential rail corridors relies on the actual &quot;costs&quot; associated with each corridor and trying to minize the total cost of the system.
+        </P>
+        <P>
+        Early in the project, I realized the importance of being able to visualize what is otherwise an abstract idea when represented as a dataset of coordinates and metrics. I used GeoPandas and Matplotlib to create
+        visualizations of the rail systems and cities, using GIS files and Shapely to map out the system over the U.S.
+        The solution is represented in a csv table containing the ideal rail corridors in order of construction that was then visualized as below.
+        </P>
+  
+        <Center my={6}>
+          <Image src="/images/works/hsr.png" alt="cm" />
         </Center>
       </Container>
     </Layout>
