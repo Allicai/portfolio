dc25bc2bdcb5f6829d296dcb8adbe055458ba0a3
Vin <vinitp0310@gmail.com>
2024-09-24 10:30:26 -0400

    clean page

--- pages/index.js
+++ pages/index.js
@@ -154,10 +154,6 @@ const Home = () => {
           </BioSection>
           <BioSection>
             <BioYear>2022</BioYear>
-            First time winning NU&apos;s <Link href="/images/intra_soc.jpeg" target="_blank" colorScheme={'teal'}>intramural soccer league</Link>.
-          </BioSection>
-          <BioSection>
-            <BioYear>2022</BioYear>
             Helped a friend design and launch a <Link href="https://hoosky.store/" target="_blank" colorScheme={'teal'}>student clothing store</Link>.
             </BioSection>
           <BioSection>
@@ -178,7 +174,7 @@ const Home = () => {
           </BioSection>
           <BioSection>
             <BioYear>Now</BioYear>
-            Machine Learning Engineer intern at Snowflake❄️.
+            Machine Learning Engineer co-op at Snowflake❄️.
           </BioSection>
         </Section>
 
