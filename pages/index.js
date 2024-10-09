054558887f7707bdfa7d5b3d6513ab9d38e0f505
Vin <vinitp0310@gmail.com>
2024-10-09 19:54:49 -0400

    new song!!!!

--- pages/index.js
+++ pages/index.js
@@ -67,7 +67,7 @@ const Home = () => {
         <Heading as="h2" variant="page-title">
           Vinit Patel
         </Heading>
-        <p>Aspiring SWE | Student | Artist | Designer | NLP Enthusiast </p>
+        <p>Aspiring SWE | Tennis Player | Artist | Designer | NLP Enthusiast </p>
       </Box>
 
       {/* Profile photo */}
@@ -264,7 +264,7 @@ const Home = () => {
         <Box borderRadius="12px" overflow="hidden" width="100%" height="152px">
           <iframe
             style={{ borderRadius: '12px' }}
-            src="https://open.spotify.com/embed/track/0o73cSWmV0YJeHvzDbJDdn?utm_source=generator"
+            src="https://open.spotify.com/embed/track/6SNGzhVrisdq63Ndz58ovT?utm_source=generator"
             width="100%"
             height="152"
             allowfullscreen=""
